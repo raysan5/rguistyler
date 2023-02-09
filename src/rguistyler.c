2bd08345d5ecda3aef1d911861d6c5b51fd4a505
Ray <raysan5@gmail.com>
2023-02-09 18:38:49 +0100

    Update external lib: `tinyfiledialogs` to 3.9.0

--- src/rguistyler.c
+++ src/rguistyler.c
@@ -32,7 +32,7 @@
 *       raylib 4.2              - Windowing/input management and drawing
 *       raygui 3.5-dev          - Immediate-mode GUI controls with custom styling and icons
 *       rpng 1.0                - PNG chunks management
-*       tinyfiledialogs 3.8.8   - Open/save file dialogs, it requires linkage with comdlg32 and ole32 libs
+*       tinyfiledialogs 3.9.0   - Open/save file dialogs, it requires linkage with comdlg32 and ole32 libs
 *
 *   COMPILATION (Windows - MinGW):
 *       gcc -o rguistyler.exe rguistyler.c external/tinyfiledialogs.c -s -O2 -std=c99 -DPLATFORM_DESKTOP
