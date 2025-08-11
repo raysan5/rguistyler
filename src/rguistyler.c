9d9dce409525ab7aa2e0673a3ff22f2258c4620f
Ray <raysan5@gmail.com>
2025-08-11 20:32:22 +0200

    Update to latest raygui

--- src/rguistyler.c
+++ src/rguistyler.c
@@ -94,7 +94,7 @@
 *       On PLATFORM_ANDROID and PLATFORM_WEB system file dialogs are not available
 *
 *   DEVELOPERS:
-*       Ramon Santamaria (@raysan5):    Supervision, review, redesign, update and maintenance.
+*       Ramon Santamaria (@raysan5):    Supervision, review, redesign, update and maintenance
 *       Adria Arranz (@Adri102):        Developer and designer (v2.0 - May.2018)
 *       Jordi Jorba (@KoroBli):         Developer and designer (v2.0 - May.2018)
 *       Sergio Martinez (@anidealgift): Developer and tester (v1.0 - 2015..2017)
